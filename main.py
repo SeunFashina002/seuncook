c75ce653a76d6134fb928bb67c7e72ab8dd1741b
Seun Fashina <fashinaoluwaseun36@gmail.com>
2024-08-04 11:39:52 +0100

    fixed bug

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import tweepy
 import time
 import google.generativeai as genai
 from dotenv import load_dotenv
-from config.x_config import api
-
 
 load_dotenv()
+
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
+
+auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
@@ -50,7 +57,7 @@ class MyStream(tweepy.StreamingClient):
                 original_text = original_tweet.text
 
                 # Generate a response using Gemini AI
-                comeback = cook()
+                comeback = cook(original_text)
 
                 # Reply to the original tweet with the generated roast
                 api.update_status(
@@ -79,7 +86,7 @@ class MyStream(tweepy.StreamingClient):
 
 # Initialize and start the stream
 bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
-stream = MyStream(bearer_token=bearer_token)
+stream = MyStream(bearer_token=BEARER_TOKEN)
 
 stream.add_rules(tweepy.StreamRule("@seuncook"))
 stream.filter()
